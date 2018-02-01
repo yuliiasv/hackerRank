68be86a4c266e4fa44727f26954655abc8c2d5b7
Yuliia Salaman <yuliia.salaman@gmail.com>
2018-02-01 22:30:35 +0200

    delete additional spaces

--- difference-two-list.py
+++ difference-two-list.py
@@ -3,12 +3,11 @@ secondList = list(range(6))
 
 sumList = sum(firstList)
 sumList2 = sum(secondList)
-print(sumList2-sumList)
+print(sumList2 - sumList)
 
-for i in firstList: #[0,1,2,3,4] i =0
-    for j in secondList:#[0,1,2,3,4,5] j =0
+for i in firstList:  # [0,1,2,3,4] i =0
+    for j in secondList:  # [0,1,2,3,4,5] j =0
         if i == j:
-            i = i + 1
+            i += 1
         else:
-            print (j)
-            
+            print(j)
